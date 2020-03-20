1de210393e3053e69b1ebe8b1943a17ace9b3ff7
Keturah Howard <keturah.dev@gmail.com>
2020-03-20 11:11:02 -0700

    make stylists clickable, display their details with details page

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -32,5 +32,11 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Stylist selectedStylist = _db.Stylists.FirstOrDefault(stylist => stylist.Id == id);
+      return View(selectedStylist);
+    }
   }
 }
