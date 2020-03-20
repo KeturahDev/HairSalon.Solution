2ff304fa34a9f3f1c2a208a2a6a49d195687dcc7
Keturah Howard <keturah.dev@gmail.com>
2020-03-20 13:23:47 -0700

    attempting to correct bug where stylist id is becoming assigned to clients Id

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -2,12 +2,13 @@ using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Controllers
 {
   public class ClientController : Controller
-//controller class names have to be singular of the models to commect with Views!
+//controller class names have to be singular of the models to connect with Views!
   {
     private readonly HairSalonContext _db;
 
@@ -19,8 +20,18 @@ namespace HairSalon.Controllers
     public ActionResult Create(int id)
     {
       Stylist stylistOfClient = _db.Stylists.FirstOrDefault(stylist => stylist.Id == id);
+      Console.WriteLine("stylists id = "+ stylistOfClient.Id);
       ViewBag.Stylist = stylistOfClient;
       return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      Console.WriteLine("clients id = "+ client.Id);
+      _db.SaveChanges();
+      return RedirectToAction("Details", "Stylist", new {id = client.StylistId});
     }
   }
 }
